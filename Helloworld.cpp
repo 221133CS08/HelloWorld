42316afc80949d2aa0d9c45b2d4df04e2ed9bf31
221133CS08 <113499026+221133CS08@users.noreply.github.com>
2022-09-16 11:58:07 +0500

    Update Helloworld.cpp

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -4,3 +4,15 @@ int main() {
     std::cout << "Hello World!";
     return 0;
 }
+
+sing namespace std;
+
+int main() {    
+    int number;
+
+    cout << "Enter an integer: ";
+    cin >> number;
+
+    cout << "You entered " << number;    
+    return 0;
+}
