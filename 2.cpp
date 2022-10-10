a90d60f58e21e9ad69e0b5e5fcbdf8405b9e7184
221133CS08 <113499026+221133CS08@users.noreply.github.com>
2022-10-10 14:04:04 +0500

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main(int argc, const char * argv[]) {
+    //problem 1
+//    int a;
+//    cout << "input number: ";
+//    cin >> a;
+//
+//    switch(a) {
+//    case 1:
+//            cout<<"Monday \n";
+//    break;
+//    case 2:
+//            cout<<"Tuesday \n";
+//    break;
+//    case 3:
+//            cout<<"Wednesday \n";
+//    break;
+//    case 4:
+//            cout<<"Thursday \n";
+//    break;
+//    case 5:
+//            cout<<"Friday \n";
+//    break;
+//    case 6:
+//            cout<<"Saturday";
+//    break;
+//    case 7:
+//            cout<<"Sunday";
+//    break;
+//    default:
+//            cout<<"Error \n";
+//    }
