b53d62f94b45e93a40182969784b74a3e606ba5b
221133CS08 <113499026+221133CS08@users.noreply.github.com>
2022-10-10 14:03:15 +0500

    Create project.cpp

---
+++ project.cpp
@@ -0,0 +1,75 @@
+//
+//  main.cpp
+//  Akfa
+//
+//  Created by Abdug'ani Shukurov on 30/09/22.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+//    int a ;
+//    int b;
+//    cout <<" Number_1 \n" ;
+//    cin >> a;
+//    cout <<" Number_1 \n" ;
+//    cin >> b;
+//    int sum = a-b;
+//    int sum_0 = a+b;
+//    int sum_1 = a*b;
+//    int sum_2 = a/b;
+//    cout << sum <<"\n" << sum_0 <<"\n" << sum_1 <<"\n" <<sum_2 <<"\n" ;
+   
+
+    //probelm 2
+//
+//    cout << "input radius(float): ";
+//    float Radius;
+//    cin >> Radius;
+//    float diametr = Radius*2;
+//    float circumference = 2*3.14*Radius;
+//    float area = 3.14*(Radius*Radius);
+//    cout << "Diametr: " << diametr << "\n";
+//    cout << "Circumference: " << circumference << "\n";
+//    cout << "Area: " << area<<"\n";
+//
+
+    
+//    //problem 3
+//    cout << "input temperature(Celsius) \n";
+//    int Celsius ;
+//    cin >> Celsius;
+//    float Fahrenheit = (Celsius * (9/5)+32);
+//    cout << "Temperature(Fahrenheit):  "<< Fahrenheit <<"\n";
+//
+//    //probelem 4
+//    int students;
+//    cout << "How many students? \n";
+//    cin >> students;
+//    int groups = students/24;
+//    int a = students%24;
+//    int free_computers = 24-a;
+//    cout << "Computers-24 \n";
+//    cout << "Groups- " << groups <<"\n";
+//    cout << "free_computers - " << free_computers <<"\n";
+//
+//  //problem5
+    int number_1;
+    int number_2;
+    cout << "Input number 1 \n";
+    cin >>number_1;
+    cout << "Input number 2 \n";
+    cin >>number_2;
+    if (number_1>number_2) {
+        cout << 1 <<"\n";
+    }
+    else{
+        cout << 0 <<"\n";
+    }
+    
+    
+    
+    
+    
+    return 0;
+}
